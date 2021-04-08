0c0e396a3b70b1dd0a030fa052367851eeb52372
Janavi Panchal <janavip81@gmail.com>
2021-04-08 11:41:07 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:Ucoe/Screens/Login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
 
